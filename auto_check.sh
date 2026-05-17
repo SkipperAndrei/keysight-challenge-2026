cb5bc3fa81625642a0bc04d207db2d8c4b6f4f7f
SkipperAndrei <giurgiu.andrei05@gmail.com>
2026-05-17 17:29:46 +0300

    Add permanent session in Docker

--- auto_check.sh
+++ auto_check.sh
@@ -8,4 +8,4 @@ cd setup
 ./stop_container.sh
 ./start-container.sh
 CONTAINER_ID=$(docker ps --format "{{.ID}}" | head -n 1)
-docker exec -u root "$CONTAINER_ID" /bin/bash -c "cd challenge-app/netem && chmod +x run.sh && ./run.sh"
+docker exec -it -u root "$CONTAINER_ID" /bin/bash -c "cd challenge-app/netem && chmod +x run.sh && ./run.sh; exec /bin/bash"
